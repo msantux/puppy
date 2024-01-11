e294a2084109a5b365e32d4f31b4f9233bedacc0
msantux <marcelo@santux.com>
2024-01-11 12:40:02 -0300

    11-Jan-2024

--- puppy.go
+++ puppy.go
@@ -23,5 +23,9 @@ func BigBarks() string {
 }
 
 func From11() {
-	fmt.Println("I'm from version 1.1")
+	fmt.Println("I'm from version 1.1.0")
+}
+
+func From12() {
+	fmt.Println("I'm from version 1.2.0")
 }
